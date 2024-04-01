fc4807e2546f88dba08e213727f7710f7546dbe9
Thiago Suchorski <suchorski@ymail.com>
2024-04-01 15:43:22 -0300

    Add option to switch to accept users existing from AD

--- src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
+++ src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
@@ -23,6 +23,7 @@ public class AplicacaoController {
 	public static final String VERSION = "1.0.2";
 	public static final long OPCAO_ID = 1;
 	public static final String OM = "gapls";
+	public static final boolean ENABLED_FROM_AD = true;
 
 	private OpcaoDAO opcaoDAO;
 
@@ -81,6 +82,10 @@ public class AplicacaoController {
 	
 	public String getOmPng() {
 		return String.format("%s.png", OM);
+	}
+	
+	public boolean getEnabledFromAd() {
+		return ENABLED_FROM_AD;
 	}
 
 }
