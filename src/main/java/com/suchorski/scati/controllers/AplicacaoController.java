71c32b7ce8003aeea5d944553371590f8950b5a3
Thiago Suchorski <suchorski@ymail.com>
2024-04-01 15:36:31 -0300

    Fix: get PNG filename from AplicacaoController

--- src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
+++ src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
@@ -22,6 +22,7 @@ public class AplicacaoController {
 	public static final String TITLE = "Sistema de Controle de Acesso de TI";
 	public static final String VERSION = "1.0.2";
 	public static final long OPCAO_ID = 1;
+	public static final String OM = "gapls";
 
 	private OpcaoDAO opcaoDAO;
 
@@ -72,6 +73,14 @@ public class AplicacaoController {
 
 	public long getOpcaoId() {
 		return OPCAO_ID;
+	}
+	
+	public String getOm() {
+		return OM;
+	}
+	
+	public String getOmPng() {
+		return String.format("%s.png", OM);
 	}
 
 }
