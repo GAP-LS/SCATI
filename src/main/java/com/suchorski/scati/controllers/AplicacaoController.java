f989c4ec623e4fc196848b091593304056f0e908
Thiago Suchorski <suchorski@ymail.com>
2024-04-01 15:48:18 -0300

    Switch default value of ENABLED_FROM_AD to false

--- src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
+++ src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
@@ -23,7 +23,7 @@ public class AplicacaoController {
 	public static final String VERSION = "1.0.3";
 	public static final long OPCAO_ID = 1;
 	public static final String OM = "gapls";
-	public static final boolean ENABLED_FROM_AD = true;
+	public static final boolean ENABLED_FROM_AD = false;
 
 	private OpcaoDAO opcaoDAO;
 
