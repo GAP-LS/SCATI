84437d9573cfb21357d3c47546d232ee6a9a83e5
Thiago Suchorski <suchorski@ymail.com>
2024-04-04 08:18:35 -0300

    Update version information

--- src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
+++ src/main/java/com/suchorski/scati/controllers/AplicacaoController.java
@@ -20,7 +20,7 @@ public class AplicacaoController {
 	public static final String CONTEXT = "scati";
 	public static final String NAME = "SCATI";
 	public static final String TITLE = "Sistema de Controle de Acesso de TI";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final long OPCAO_ID = 1;
 	public static final String OM = "gapls";
 	public static final boolean ENABLED_FROM_AD = false;
